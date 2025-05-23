0df7a14f33223e577b04880a7bf259f87db366fb
Yosef-16 <yosefyeshitila211@gmail.com>
2025-05-23 22:19:16 +0300

    first commit

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,3 @@
-let firstLine = 'C';
-console.log  (firstLine);
-let secondLine = 'Python is cool';
-console.log (secondLine);
-let ThirdLine = 'JavaScript is amazing';
-console.log (ThirdLine);
+console.log ('C');
+console.log ('Python is cool');
+console.log ('JavaScript is amazing');
