ae8046ccd0ca9b34e34b8249753f3a905ce754e4
Yosef-16 <yosefyeshitila211@gmail.com>
2025-05-23 23:07:50 +0300

    first commit

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const x = parseInt(process.argv[2]);
+let output = "";
+
+if (isNaN(x) || x <= 0) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    output += "C is fun\n";
+  }
+  console.log(output.trim());
+}
